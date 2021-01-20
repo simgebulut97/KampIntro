df749ba3dde4719ba9b71041589219fb6de39c72
Simge Bulut <26049382@arcelik.com>
2021-01-20 21:54:05 +0300

    Array alıp gösteren listeleme fonksiyonu eklendi.

--- metodlar/Program.cs
+++ metodlar/Program.cs
@@ -6,7 +6,49 @@ namespace metodlar
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string urunAdi = "Elma";
+            double fiyati = 15;
+            string aciklama = "Amasya elmasi";
+            string[] meyveler = new string[] { };
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya elmasi";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakir karpuzu";
+
+            Urun[] urunler = new Urun[] { urun1,urun2};
+
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("------------------------------");
+            }
+            Console.WriteLine("-------------Metodlar-------------------");
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Elma","Yeşil",12);
+
+            sepetManager.Ekle2("Armut","Deveci",9);
+
+
+
+
+            {
+
+            }
+
+
         }
     }
 }
---
+++ metodlar/SepetManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace metodlar
+{
+    class SepetManager
+    {
+        //naming convention E büyük
+        public void Ekle(Urun urun) 
+        {
+
+            Console.WriteLine("Sepete eklendi." + urun.Adi);
+
+        }
+        public void Ekle2(string urunAdi, string Aciklama, double fiyat)
+        {
+            Console.WriteLine("Sepete eklendi." + urun.Adi);
+        }
+    }
+}
