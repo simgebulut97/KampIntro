8497c4f71a5610320fcf12ef0bc6a7d4dd00e519
Simge Bulut <26049382@arcelik.com>
2021-01-21 19:49:16 +0300

    Generics

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Simge");
+            Console.WriteLine(isimler.Length);
+            isimler.Add("Berker");
+            Console.WriteLine(isimler.Length);
+            foreach (var isim in isimler.Items)
+            {
+                Console.WriteLine(isim);
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
