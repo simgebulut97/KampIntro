463bd3dd4836d753541b913db0a4619e1e87de53
Simge Bulut <26049382@arcelik.com>
2021-01-24 00:06:40 +0300

    Interface yapısına hazırlık

---
+++ OOP2/MusteriManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    //Is siniflari
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine(musteri+" eklendi");
+        }
+    }
+}
---
+++ OOP2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Simge";
+            musteri1.Soyadi = "Bulut";
+            musteri1.TcNo = "123456789";
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.MusteriNo = "98765";
+            musteri2.Id = 2;
+            musteri2.SirketAdi = "kodlama.io";
+            musteri2.VergiNo = "1234";
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(musteri1);
+            musteriManager.Add(musteri2);
+
+
+
+        }
+    }
+}
