947e7139365311f64366ad76f3504b154f033d7b
Simge Bulut <26049382@arcelik.com>
2021-01-23 20:06:53 +0300

    Dictionary Yapısı

---
+++ Hafta2Odev5/MyDictionary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hafta2Odev5
+{
+    class MyDictionary<key,value>
+    {
+        key[] Ids;
+        value[] Names;
+
+        public MyDictionary()
+        {
+            Ids = new key[0];
+            Names = new value[0];
+
+        }
+        public void Add(key Id,value Name)
+        {
+            key[] tempArray = Ids;
+            Ids = new key[Ids.Length + 1];
+            value[] tempArrayV = Names;
+            Names = new value[Names.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                Ids[i] = tempArray[i];
+            }
+            Ids[Ids.Length - 1] = Id;
+            for (int i = 0; i < tempArrayV.Length; i++)
+            {
+                Names[i] = tempArrayV[i];
+            }
+            Names[Names.Length - 1] = Name;
+            Console.WriteLine(Id + " " + Name + " :Başarıyla Eklendi!");
+        }
+    
+        
+    }
+
+}
---
+++ Hafta2Odev5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Hafta2Odev5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> ogrenciler = new MyDictionary<int, string>();
+            ogrenciler.Add(323, "Simge");
+            ogrenciler.Add(326, "Asli");     
+            ogrenciler.Add(909, "Gokce");
+
+
+        }
+    }
+}
