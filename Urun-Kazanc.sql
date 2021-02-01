7ab2aa808a0d5cd70cfc0dedac8c597e9cd51a2e
Simge Bulut <26049382@arcelik.com>
2021-02-01 22:55:23 +0300

    sql temel komutlarla ürünlerden elde edilen kazancın ürüne göre sıralaması

---
+++ Urun-Kazanc.sql
@@ -0,0 +1,5 @@
+﻿select products.productName as ÜrünAdı, sum([Order Details].Quantity * [Order Details].UnitPrice) as KazanılanToplamMiktar from Products
+inner join [Order Details] on [Order Details].ProductID = Products.ProductID inner join Orders
+on Orders.OrderID=[Order Details].OrderID
+group by Products.ProductName 
+order by Products.ProductName 
